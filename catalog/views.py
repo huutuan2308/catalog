3dd551016379c3c7a4be509d67c363872ea0b1e2
HuuTuan <huutuan238@gmail.com>
2020-08-07 16:04:28 +0700

    Django Tutorial Part 6:Bai 6
    Django Tutorial Part 5: Creating our home page
    
    Django Tutorial Part 6: Generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -24,3 +24,57 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+from django.shortcuts import get_object_or_404
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    num_genres=Genre.objects.all().count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres':num_genres,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.all()
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+    context_object_name = 'author_list'   # your own name for the list as a template variable
+    queryset = Author.objects.all()
+    template_name = 'author/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    def author_detail_view(request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': author})
+
